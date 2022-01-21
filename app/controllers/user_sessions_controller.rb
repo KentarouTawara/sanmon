4b79a7c5130ebfe52ff753e647d6a5e028944df5
KentarouTawara <sironagasuken2@gmail.com>
2022-01-21 18:22:51 +0900

    add login feature

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,21 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login
+
+  def new;end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Login failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, status: :see_other
+  end
+end
