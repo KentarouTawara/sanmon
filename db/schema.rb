90d2a7230d3cf3971f6355112c1058d41ad88ecd
KentarouTawara <sironagasuken2@gmail.com>
2022-01-22 15:34:57 +0900

    add random words

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_062559) do
+ActiveRecord::Schema.define(version: 2022_01_22_062334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2022_01_21_062559) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "start_at", precision: 6
+    t.datetime "end_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "works", force: :cascade do |t|
