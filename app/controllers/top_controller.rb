86c8da1be129a2675cd40b190b3c194e8883614e
KentarouTawara <sironagasuken2@gmail.com>
2022-01-26 16:31:13 +0900

    add words

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   skip_before_action :require_login
+
   def index
+    @today_words = Word.today_three_words
   end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,20 @@
 class Word < ApplicationRecord
   validates :name, presence: true
+
+  scope :today_three_words, ->() {
+    where("start_at >= ?", Time.zone.now.beginning_of_day)
+      .where("end_at <= ?", Time.zone.now.end_of_day)
+  }
+
+  class << self
+    def update_today_words
+      three_words = Word.order("RANDOM()").limit(3)
+      three_words.each do |word|
+        word.start_at = Time.zone.now.beginning_of_day
+        word.end_at = Time.zone.now.end_of_day
+        word.save!
+      end
+    end
+
+  end
 end
