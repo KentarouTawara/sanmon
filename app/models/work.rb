965b4cc67bc4c1621011da09115782acbfce9c9f
KentarouTawara <sironagasuken2@gmail.com>
2022-02-03 19:52:53 +0900

    add validation

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,10 @@
 class Work < ApplicationRecord
+  include ActiveModel::Validations
   belongs_to :user
+  has_many :random_words, dependent: :destroy
+  has_many :three_words, through: :random_words, source: :word, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { in: 100..200 }
+  validates_with WorkValidator
 end
