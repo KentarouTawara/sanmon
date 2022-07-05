8c5398d60fec07db308e9d1a2fbc5362718923de
KentarouTawara <sironagasuken2@gmail.com>
2022-07-05 23:04:59 +0900

    [add] userの編集機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,6 +23,13 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: 'ユーザー情報の更新が完了しました'
+    else
+      flash.now[:error] = 'ユーザー情報の更新に失敗しました'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
